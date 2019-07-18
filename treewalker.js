ae39fe030580713e476e29b31e71e44bc3d9e548
Sangjin Yun <sangjin.yun@sk.com>
2019-07-18 22:12:53 +0900

    link node만 분석하도록 변경.
    ilbe.com 링크는 무시하도록 변경.

--- treewalker.js
+++ treewalker.js
@@ -1,7 +1,7 @@
 var showAlways = false;
 
 var nojNode = document.createElement("div");
-var stringContainingXMLSource = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:se=\"http://svg-edit.googlecode.com\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" width=\"40.699252002066174\" height=\"26.443611363746157\" style=\"\">                                    <title>my vector image</title>                                    <!-- Created with Vector Paint - http://www.vectorpaint.yaks.com/ https://chrome.google.com/webstore/detail/hnbpdiengicdefcjecjbnjnoifekhgdo -->                                    <rect id=\"backgroundrect\" width=\"100%\" height=\"100%\" x=\"0\" y=\"0\" fill=\"#FFFFFF\" stroke=\"none\"/>                                <g class=\"currentLayer\" style=\"\"><title>Layer 1</title><foreignObject fill=\"#4a90d6\" stroke=\"#222222\" stroke-width=\"2\" stroke-linejoin=\"round\" stroke-dashoffset=\"\" fill-rule=\"nonzero\" font-size=\"25\" font-family=\"Arial, Helvetica, sans-serif\" letter-spacing=\"0\" word-spacing=\"0\" marker-start=\"\" marker-mid=\"\" marker-end=\"\" id=\"svg_2\" x=\"0.636174913415124\" y=\"1.6111021080296724\" width=\"35.71619886003289\" height=\"24.397168879087456\" style=\"color: rgb(0, 0, 0);\" class=\"\" font-weight=\"bold\"><p style=\"border: none;outline: none;font-size: inherit;line-height: 1em;padding:0;margin:0;\" xmlns=\"http://www.w3.org/1999/xhtml\"><p xmlns=\"http://www.w3.org/1999/xhtml\" style=\"border: none;outline: none;font-size: inherit;line-height: 1em;padding:0;margin:0;\">NO</p></p></foreignObject><path fill=\"#d50b0b\" fill-opacity=\"1\" stroke=\"#d50b0b\" stroke-opacity=\"1\" stroke-width=\"2\" stroke-dasharray=\"none\" stroke-linejoin=\"round\" stroke-linecap=\"butt\" stroke-dashoffset=\"\" fill-rule=\"nonzero\" opacity=\"1\" marker-start=\"\" marker-mid=\"\" marker-end=\"\" d=\"M20.397791550650048,13.807156531908547 C20.397791550650048,8.85473178382083 24.138905548120736,4.843267737869773 28.75756480425739,4.843267737869773 C33.37622406039405,4.843267737869773 37.11733805786473,8.85473178382083 37.11733805786473,13.807156531908547 C37.11733805786473,18.759581279996265 33.37622406039405,22.771045325947323 28.75756480425739,22.771045325947323 C24.138905548120736,22.771045325947323 20.397791550650048,18.759581279996265 20.397791550650048,13.807156531908547 z\" id=\"svg_5\" class=\"\"/></g></svg>"
+var stringContainingXMLSource = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:se=\"http://svg-edit.googlecode.com\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" width=\"40.699252002066174\" height=\"26.443611363746157\" style=\"\">                                    <title>my vector image</title>                                    <!-- Created with Vector Paint - http://www.vectorpaint.yaks.com/ https://chrome.google.com/webstore/detail/hnbpdiengicdefcjecjbnjnoifekhgdo -->                                    <rect id=\"backgroundrect\" width=\"100%\" height=\"100%\" x=\"0\" y=\"0\" fill=\"#FFFFFF\" stroke=\"none\"/>                                <g class=\"currentLayer\" style=\"\"><title>No Japan</title><foreignObject fill=\"#4a90d6\" stroke=\"#222222\" stroke-width=\"2\" stroke-linejoin=\"round\" stroke-dashoffset=\"\" fill-rule=\"nonzero\" font-size=\"25\" font-family=\"Arial, Helvetica, sans-serif\" letter-spacing=\"0\" word-spacing=\"0\" marker-start=\"\" marker-mid=\"\" marker-end=\"\" id=\"svg_2\" x=\"0.636174913415124\" y=\"1.6111021080296724\" width=\"35.71619886003289\" height=\"24.397168879087456\" style=\"color: rgb(0, 0, 0);\" class=\"\" font-weight=\"bold\"><p style=\"border: none;outline: none;font-size: inherit;line-height: 1em;padding:0;margin:0;\" xmlns=\"http://www.w3.org/1999/xhtml\"><p xmlns=\"http://www.w3.org/1999/xhtml\" style=\"border: none;outline: none;font-size: inherit;line-height: 1em;padding:0;margin:0;\">NO</p></p></foreignObject><path fill=\"#d50b0b\" fill-opacity=\"1\" stroke=\"#d50b0b\" stroke-opacity=\"1\" stroke-width=\"2\" stroke-dasharray=\"none\" stroke-linejoin=\"round\" stroke-linecap=\"butt\" stroke-dashoffset=\"\" fill-rule=\"nonzero\" opacity=\"1\" marker-start=\"\" marker-mid=\"\" marker-end=\"\" d=\"M20.397791550650048,13.807156531908547 C20.397791550650048,8.85473178382083 24.138905548120736,4.843267737869773 28.75756480425739,4.843267737869773 C33.37622406039405,4.843267737869773 37.11733805786473,8.85473178382083 37.11733805786473,13.807156531908547 C37.11733805786473,18.759581279996265 33.37622406039405,22.771045325947323 28.75756480425739,22.771045325947323 C24.138905548120736,22.771045325947323 20.397791550650048,18.759581279996265 20.397791550650048,13.807156531908547 z\" id=\"svg_5\" class=\"\"/></g></svg>"
 var parser = new DOMParser();
 
 if (showAlways == false) {
@@ -9,22 +9,11 @@ if (showAlways == false) {
 	nojNode.append(imgDom.documentElement);
 	nojNode.style.display = "none";
 	document.body.appendChild(nojNode);
-}
-
-function findTag(node) {
-	var foundTag = false;
-	while (node != null) {
-		if (node.tagName != null 
-				&& (node.tagName.startsWith("H")  
-					|| node.tagName == "A" 
-					|| node.tagName == "SPAN")) {
-			return node;
-		} else {
-			node = node.parentElement;
-		}
-	}
-	
-	return null;
+} else {
+	nojNode.style.top = '0px';
+	nojNode.style.left = '0px';
+	nojNode.style.position = "absolute";
+	document.body.append(nojNode);
 }
 
 function ShowDiv(element) {
@@ -51,7 +40,7 @@ function HideDiv(element) {
 function onLoad() {
 	// Here starts the bit for your content script
 	var list = [
-		"ABC마트‎",
+		"ABC.*마트", "ABC.*MART‎",
 		"롯데",
 		"유니클로", "UNIQLO",
 		"소니", "SONY",
@@ -109,7 +98,6 @@ function onLoad() {
 		"요넥스",
 		"유니클로",
 		"무인양품", "MUJI",
-		"ABC 마트",
 		"겐조",
 		"요지 야마모토",
 		"Y-3",
@@ -169,33 +157,41 @@ function onLoad() {
     "갸스비",
     "우루오스"
 	]
-	var re = new RegExp(`${list.join("|")}`, 'gi');
+	var re = new RegExp(`${list.join("|")}`, 'gmi');
 	var regs;
 
-	var walker = document.createTreeWalker(
-		document.body, NodeFilter.SHOW_TEXT, function (node) {
-			if ((regs = re.exec(node.textContent))) {
-				
-				if (showAlways) {
-					var div = findTag(node);
-					
-					if (div != null && div.addedNoj != true) {
-						var nojNode = document.createElement("div");
-						var imgDom = parser.parseFromString(stringContainingXMLSource, "image/svg+xml");
-						var imgNode = imgDom.documentElement;
-						div.append(imgNode);
-						div.addedNoj = true;
-					}
-				} else {
-					node.parentElement.onmouseover = function () { ShowDiv(this); };
-					node.parentElement.onmouseout = function () { HideDiv(this); };		
+	var links = document.body.getElementsByTagName("A");
+	for (var i = 0; i < links.length; i++) {
+		var node = links[i];
+		if (node.href != null && node.href.indexOf("ilbe.com") != -1) {
+			node.href = "#";
+		}
+		
+		if ((regs = re.exec(node.textContent))) {
+			var div = node;
+			
+			if (showAlways) {
+				if (div != null && div.addedNoj != true) {
+					var divNode = document.createElement("div");
+					var imgDom = parser.parseFromString(stringContainingXMLSource, "image/svg+xml");
+					var imgNode = imgDom.documentElement;
+					var viewportOffset = div.getBoundingClientRect();
+					divNode.style.top = viewportOffset.top + 'px';
+					divNode.style.left = viewportOffset.left - 0 + 'px';
+					divNode.style.top = 0 + 'px';
+					divNode.style.left = 0 + 'px';
+					divNode.style.position = "absolute"
+					divNode.append(imgNode);
+					divNode.text = "node	.textContent";
+					div.append(divNode);
+					div.addedNoj = true;
 				}
+			} else {
+				node.parentElement.onmouseover = function () { ShowDiv(this); };
+				node.parentElement.onmouseout = function () { HideDiv(this); };		
 			}
-			return NodeFilter.FILTER_SKIP;
-		}, false);
-
-	// Make the walker step through the nodes
-	walker.nextNode();
+		}
+	}
 }
 
 onLoad();
